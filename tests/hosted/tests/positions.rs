6bb9a12e673cb47e620eb364a3490d7f79e0a076
Drew Nutter <dnut@users.noreply.github.com>
2022-06-08 10:28:47 -0400

    add mock adapter and refactor rust test setup to be more generic

---
+++ tests/hosted/tests/positions.rs
@@ -0,0 +1,15 @@
+// use anyhow::Result;
+// use serial_test::serial;
+
+
+// #[tokio::test]
+// #[serial]
+// async fn cannot_liquidate_healthy_user() -> Result<()> {
+//     let (ctx, env) = build_environment_with_raw_token_balances(1, 1).await?;
+
+//     // // A liquidator tries to liquidate User A, it should not be able to
+//     // let result = scen.user_a_liq.liquidate_begin().await;
+//     // assert_program_error!(ErrorCode::Healthy, result);
+
+//     Ok(())
+// }
