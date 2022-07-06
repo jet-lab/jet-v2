309f684332d5e0ad3d704be97a041f48a355bf19
Partially Sorted <91437574+partiallysorted@users.noreply.github.com>
2022-07-06 11:08:51 -0500

    Made declarative tests multi-user. (#122)
    * Made declarative tests multi-user.
    
    * Create user token accounts.
    
    * Changed test extension so tests don't run automatically.
    
    * Added ETH to scenarios.

---
+++ tests/declarative/deposit.sim.ts
@@ -0,0 +1,54 @@
+import { Account, Connection, Keypair, LAMPORTS_PER_SOL } from "@solana/web3.js"
+import * as fs from "fs"
+import * as os from "os"
+
+import { Replicant, sleep } from "./replicant"
+
+import CONFIG from "../../libraries/ts/src/margin/config.json"
+
+import TEST_CONFIG from "./scenarios/deposit.json"
+
+describe("Deposits", () => {
+  const config = CONFIG.devnet
+  const connection = new Connection(config.url, "processed")
+
+  const replicants: Replicant[] = []
+
+  it("Create users", async () => {
+    for (const userConfig of TEST_CONFIG.users) {
+      replicants.push(
+        new Replicant(TEST_CONFIG, os.homedir() + "/.config/solana/" + userConfig.keypair, "devnet", connection)
+      )
+    }
+  })
+
+  it("Fund users", async () => {
+    for (const replicant of replicants) {
+      await replicant.fundUser()
+    }
+  })
+
+  it("Load pools", async () => {
+    for (const replicant of replicants) {
+      await replicant.loadPools()
+    }
+  })
+
+  it("Create margin accounts", async () => {
+    for (const replicant of replicants) {
+      await replicant.createAccounts()
+    }
+  })
+
+  it("Process deposits", async () => {
+    for (const replicant of replicants) {
+      await replicant.processDeposits()
+    }
+  })
+
+  it("Close margin accounts", async () => {
+    for (const replicant of replicants) {
+      await replicant.closeAccounts()
+    }
+  })
+})
