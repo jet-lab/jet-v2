580049ea69612af81fdac5f0c0fa1e7fd8944652
eyevzz <norman@jetprotocol.io>
2022-07-22 09:20:44 +0100

    Removes fee collection threshold

--- tests/hosted/tests/sanity.rs
+++ tests/hosted/tests/sanity.rs
@@ -30,7 +30,6 @@ const DEFAULT_POOL_CONFIG: MarginPoolConfig = MarginPoolConfig {
     utilization_rate_1: 10,
     utilization_rate_2: 20,
     management_fee_rate: 10,
-    management_fee_collect_threshold: 100,
     flags: PoolFlags::ALLOW_LENDING.bits(),
 };
 
