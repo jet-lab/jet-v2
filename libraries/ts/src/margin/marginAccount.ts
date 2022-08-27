faedc073a863a8b6258a674bca4e234f3fa9744c
Wakahisa <nevilledips@gmail.com>
2022-08-27 05:59:02 -0400

    Update libraries/ts/src/margin/marginAccount.ts
    Co-authored-by: Tristyn Stimpson <tristynstimpson@gmail.com>

--- libraries/ts/src/margin/marginAccount.ts
+++ libraries/ts/src/margin/marginAccount.ts
@@ -523,7 +523,7 @@ export class MarginAccount {
     const repay = TokenAmount.min(loanBalance, walletAmount)
 
     // Max swap
-    const swap = (TokenAmount.max(depositBalance, pool.vault)).add(borrow)
+    const swap = TokenAmount.min(depositBalance.add(borrow), pool.vault)
 
     // Max transfer
     const transfer = withdraw
