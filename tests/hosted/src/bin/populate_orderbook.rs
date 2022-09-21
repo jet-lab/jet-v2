a011f18f4ef3a364abb7e5445ee070f61f9327fe
zachanon <zachanon@gmail.com>
2022-09-21 22:20:28 +0700

    populate orderbook

--- tests/hosted/src/bin/populate_orderbook.rs
+++ tests/hosted/src/bin/populate_orderbook.rs
@@ -2,6 +2,8 @@ use anchor_lang::AccountDeserialize;
 use anyhow::Result;
 use jet_bonds::control::state::BondManager;
 use jet_margin_sdk::bonds::{BondsIxBuilder, OrderParams};
+use jet_proto_math::fixed_point::{Fp32, FP32_ONE};
+use rand::{thread_rng, Rng};
 use solana_client::{rpc_client::RpcClient, rpc_config::RpcSendTransactionConfig};
 use solana_sdk::{
     commitment_config::CommitmentConfig,
@@ -25,6 +27,8 @@ const DEVNET_USDC_FAUCET: Pubkey = pubkey!("MV2QoKwWmRQnu8HY56Hsmfhb6aC6L6mLirmQ
 
 const TOKEN_AMOUNT: u64 = 10_000_000_000;
 const TICKET_AMOUNT: u64 = 5_000_000_000;
+
+const BOND_DURATION: u64 = 5;
 
 lazy_static::lazy_static! {
     static ref PAYER: String = shellexpand::env("$PWD/tests/keypairs/payer.json")
@@ -171,6 +175,31 @@ impl<'a> User<'a> {
     }
 }
 
+fn airdrop_ix(token_account: &Pubkey, faucet: &Pubkey, mint: &Pubkey, amount: u64) -> Instruction {
+    let mut data = vec![1];
+    data.extend_from_slice(&amount.to_le_bytes());
+
+    let pk_nonce = Pubkey::find_program_address(&[b"faucet"], &FAUCET_PID).0;
+    let keys = vec![
+        AccountMeta::new_readonly(pk_nonce, false),
+        AccountMeta::new(*mint, false),
+        AccountMeta::new(*token_account, false),
+        AccountMeta::new_readonly(spl_token::ID, false),
+        AccountMeta::new_readonly(*faucet, false),
+    ];
+    Instruction::new_with_bytes(FAUCET_PID, &data, keys)
+}
+
+fn rate_to_price(rate: f64, tenor: u64) -> Option<u64> {
+    let fp_rate = Fp32::upcast_fp32((rate * FP32_ONE as f64) as u64);
+    let price = Fp32::ONE / (Fp32::ONE + fp_rate * tenor);
+    price.downcast_u64()
+}
+
+fn ui_price(fp: u64) -> f64 {
+    fp as f64 / FP32_ONE as f64
+}
+
 fn main() -> Result<()> {
     let conn = RpcClient::new_with_commitment(ENDPOINT, CommitmentConfig::confirmed());
     let wallet = map_keypair_file(PAYER.clone())?;
@@ -185,34 +214,89 @@ fn main() -> Result<()> {
     alice.init_and_fund(TOKEN_AMOUNT, TICKET_AMOUNT)?;
     bob.init_and_fund(TOKEN_AMOUNT, TICKET_AMOUNT)?;
 
-    // let asks_data = &mut client.conn.get_account_data(&client.ix.asks()?)?;
-    // let asks = agnostic_orderbook::state::critbit::Slab::<jet_bonds::orderbook::state::CallbackInfo>::from_buffer(
-    //         asks_data,
-    //         agnostic_orderbook::state::AccountTag::Asks,
-    //     )?;
-    // let bids_data = &mut client.conn.get_account_data(&client.ix.bids()?)?;
-    // let bids = agnostic_orderbook::state::critbit::Slab::<jet_bonds::orderbook::state::CallbackInfo>::from_buffer(
-    //         bids_data,
-    //         agnostic_orderbook::state::AccountTag::Bids,
-    //     )?;
+    let params = |tickets, tokens, price| OrderParams {
+        max_bond_ticket_qty: tickets,
+        max_underlying_token_qty: tokens,
+        limit_price: price,
+        match_limit: 100,
+        post_only: false,
+        post_allowed: true,
+        auto_stake: true,
+    };
+    let price = |base: u64, quote: u64| (Fp32::from(quote) / base).as_u64().unwrap();
 
-    // dbg!(bids.into_iter(true).next().is_some());
-    // dbg!(asks.into_iter(true).next().is_some());
+    let mut rng = thread_rng();
+
+    for _ in 0..100 {
+        let num: f64 = rng.sample(rand_distr::StandardNormal);
+        let rate = (0.05 * num).exp();
+        let mut principal: u64 = rng.gen();
+        principal %= 100_000;
+        let interest = principal as f64 * rate * BOND_DURATION as f64;
+
+        let borrow = params(
+            principal + interest as u64,
+            u64::MAX,
+            rate_to_price(rate, BOND_DURATION).unwrap(),
+        );
+        alice.borrow_order(borrow)?;
+    }
+
+    for _ in 0..100 {
+        let num: f64 = rng.sample(rand_distr::StandardNormal);
+        let rate = (0.05 * num).exp();
+        let mut principal: u64 = rng.gen();
+        principal %= 100_000;
+
+        let lend = params(
+            u64::MAX,
+            principal,
+            rate_to_price(rate, BOND_DURATION).unwrap(),
+        );
+        bob.lend_order(lend)?;
+    }
+
+    // read and display the orderbook
+    let asks_data = &mut client.conn.get_account_data(&client.ix.asks()?)?;
+    let asks = agnostic_orderbook::state::critbit::Slab::<jet_bonds::orderbook::state::CallbackInfo>::from_buffer(
+            asks_data,
+            agnostic_orderbook::state::AccountTag::Asks,
+        )?;
+    let bids_data = &mut client.conn.get_account_data(&client.ix.bids()?)?;
+    let bids = agnostic_orderbook::state::critbit::Slab::<jet_bonds::orderbook::state::CallbackInfo>::from_buffer(
+            bids_data,
+            agnostic_orderbook::state::AccountTag::Bids,
+        )?;
+
+    #[derive(Debug)]
+    struct Order {
+        pub base: u64,
+        pub quote: u64,
+        pub price_fp32: u64,
+        pub price_f64: f64,
+    }
+    dbg!(asks
+        .into_iter(true)
+        .map(|n| Order {
+            base: n.base_quantity,
+            quote: Fp32::upcast_fp32(n.price())
+                .u64_mul(n.base_quantity)
+                .unwrap(),
+            price_fp32: n.price(),
+            price_f64: ui_price(n.price())
+        })
+        .collect::<Vec<Order>>());
+    dbg!(bids
+        .into_iter(true)
+        .map(|n| Order {
+            base: n.base_quantity,
+            quote: Fp32::upcast_fp32(n.price())
+                .u64_mul(n.base_quantity)
+                .unwrap(),
+            price_fp32: n.price(),
+            price_f64: ui_price(n.price())
+        })
+        .collect::<Vec<Order>>());
 
     Ok(())
-}
-
-fn airdrop_ix(token_account: &Pubkey, faucet: &Pubkey, mint: &Pubkey, amount: u64) -> Instruction {
-    let mut data = vec![1];
-    data.extend_from_slice(&amount.to_le_bytes());
-
-    let pk_nonce = Pubkey::find_program_address(&[b"faucet"], &FAUCET_PID).0;
-    let keys = vec![
-        AccountMeta::new_readonly(pk_nonce, false),
-        AccountMeta::new(*mint, false),
-        AccountMeta::new(*token_account, false),
-        AccountMeta::new_readonly(spl_token::ID, false),
-        AccountMeta::new_readonly(*faucet, false),
-    ];
-    Instruction::new_with_bytes(FAUCET_PID, &data, keys)
 }
