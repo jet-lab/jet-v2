47733974ec2bb0ce14a6e11c13c93866817d0686
Tristyn <tristynstimpson@gmail.com>
2022-09-26 15:28:41 -0500

    Use cargo run so we don't care if jetctl is built or not

--- libraries/rust/ctl/generate-app-configs.sh
+++ libraries/rust/ctl/generate-app-configs.sh
@@ -4,17 +4,9 @@ CFG_DEVNET=./generated/devnet.json
 CFG_MAINNET=./generated/mainnet.json
 CFG_GLOBAL=./generated/config.json.gen
 
-if [[ $(type -P "jetctl") ]]; then
-    JETCTL=jetctl
-elif [[ -f ./target/debug/jetctl ]]; then
-    JETCTL=./target/debug/jetctl
-else
-    JETCTL=./target/release/jetctl
-fi
-
 mkdir -p ./generated
-$JETCTL generate-app-config ./configs/devnet -ud -o $CFG_DEVNET
-$JETCTL generate-app-config ./configs/mainnet -um -o $CFG_MAINNET
+cargo run --bin jetctl generate-app-config ./configs/devnet -ud -o $CFG_DEVNET
+cargo run --bin jetctl generate-app-config ./configs/mainnet -um -o $CFG_MAINNET
 
 echo '{}' > $CFG_GLOBAL
 echo $(jq '.devnet = input' $CFG_GLOBAL $CFG_DEVNET) > $CFG_GLOBAL
