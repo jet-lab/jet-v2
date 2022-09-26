075e94779cfdcb77a73cbf3c8ed75d9f817c5a5d
qiqicodes <qiqicodes@users.noreply.github.com>
2022-09-26 20:12:23 +0000

    Deploying to gh-pages from @ jet-lab/jet-v2@23c37e3faa692b92f0c4c2d3ac843b9efe082d2a 🚀

--- margin-rust/source-files.js
+++ margin-rust/source-files.js
@@ -1,5 +1,6 @@
-var sourcesIndex = {};
-sourcesIndex["jet_margin"] = {"name":"","dirs":[{"name":"instructions","files":["accounting_invoke.rs","adapter_invoke.rs","close_account.rs","close_position.rs","create_account.rs","liquidate_begin.rs","liquidate_end.rs","liquidator_invoke.rs","refresh_position_metadata.rs","register_position.rs","update_position_balance.rs","verify_healthy.rs"]},{"name":"state","dirs":[{"name":"account","files":["positions.rs"]}],"files":["account.rs"]}],"files":["adapter.rs","events.rs","instructions.rs","lib.rs","state.rs","syscall.rs","util.rs"]};
-sourcesIndex["jet_margin_pool"] = {"name":"","dirs":[{"name":"instructions","files":["close_loan.rs","collect.rs","configure.rs","create_pool.rs","deposit.rs","margin_borrow.rs","margin_refresh_position.rs","margin_repay.rs","register_loan.rs","repay.rs","withdraw.rs"]}],"files":["events.rs","instructions.rs","lib.rs","state.rs","util.rs"]};
-sourcesIndex["jet_margin_swap"] = {"name":"","dirs":[{"name":"instructions","files":["margin_spl_swap.rs"]}],"files":["instructions.rs","lib.rs"]};
+var sourcesIndex = JSON.parse('{\
+"jet_margin":["",[["instructions",[],["accounting_invoke.rs","adapter_invoke.rs","close_account.rs","close_position.rs","create_account.rs","liquidate_begin.rs","liquidate_end.rs","liquidator_invoke.rs","refresh_position_metadata.rs","register_position.rs","update_position_balance.rs","verify_healthy.rs"]],["state",[["account",[],["positions.rs"]]],["account.rs"]]],["adapter.rs","events.rs","instructions.rs","lib.rs","state.rs","syscall.rs","util.rs"]],\
+"jet_margin_pool":["",[["instructions",[],["close_loan.rs","collect.rs","configure.rs","create_pool.rs","deposit.rs","margin_borrow.rs","margin_refresh_position.rs","margin_repay.rs","register_loan.rs","repay.rs","withdraw.rs"]]],["events.rs","instructions.rs","lib.rs","state.rs","util.rs"]],\
+"jet_margin_swap":["",[["instructions",[],["margin_spl_swap.rs"]]],["instructions.rs","lib.rs"]]\
+}');
 createSourceSidebar();
