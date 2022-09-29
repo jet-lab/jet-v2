e2b91283bc5c9d672afe1e6af5153166b9d31f85
zachanon <zachanon@gmail.com>
2022-09-29 15:55:57 +0700

    restore ctl

---
+++ libraries/rust/ctl/src/actions/margin.rs
@@ -0,0 +1,362 @@
+use std::{
+    fmt::Display,
+    str::FromStr,
+    time::{SystemTime, UNIX_EPOCH},
+};
+
+use anchor_lang::AccountDeserialize;
+use anyhow::{bail, Result};
+use clap::Parser;
+use comfy_table::{presets::UTF8_FULL, Table};
+use futures::FutureExt;
+use jet_margin_sdk::{
+    ix_builder::{get_metadata_address, ControlIxBuilder},
+    jet_margin::{self, MarginAccount, PriceInfo, Valuation},
+    jet_margin_pool::{self, MarginPool},
+    jet_metadata,
+};
+use serde::{Deserialize, Serialize};
+use serde_with::{serde_as, DisplayFromStr};
+use solana_sdk::{pubkey, pubkey::Pubkey};
+
+use crate::{
+    client::{Client, Plan},
+    governance::resolve_payer,
+};
+
+static TOKEN_BLACKLIST: &[Pubkey] = &[
+    pubkey!("EDg8oL2FoK7pmCvR8iRGmbW2HioUaDNWRihkQA5rEqDj"),
+    pubkey!("J9n7rmgGgaiysVGnspchnCk4riWC9qDeh9wp8rc2PzGG"),
+];
+
+#[serde_as]
+#[derive(Debug, Parser, Serialize, Deserialize)]
+pub struct TokenConfig {
+    /// The mint address for the token being configured
+    #[serde_as(as = "DisplayFromStr")]
+    pub mint: Pubkey,
+
+    #[clap(long)]
+    #[serde_as(as = "Option<DisplayFromStr>")]
+    pub pyth_price: Option<Pubkey>,
+
+    #[clap(long)]
+    #[serde_as(as = "Option<DisplayFromStr>")]
+    pub pyth_product: Option<Pubkey>,
+
+    #[clap(long)]
+    #[serde_as(as = "Option<DisplayFromStr>")]
+    pub token_kind: Option<TokenKind>,
+
+    #[clap(long)]
+    pub collateral_weight: Option<u16>,
+
+    #[clap(long)]
+    pub max_leverage: Option<u16>,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone, Copy)]
+pub enum TokenKind {
+    NonCollateral,
+    Collateral,
+    Claim,
+}
+
+impl From<TokenKind> for jet_metadata::TokenKind {
+    fn from(val: TokenKind) -> Self {
+        match val {
+            TokenKind::NonCollateral => jet_metadata::TokenKind::NonCollateral,
+            TokenKind::Collateral => jet_metadata::TokenKind::Collateral,
+            TokenKind::Claim => jet_metadata::TokenKind::Claim,
+        }
+    }
+}
+
+impl FromStr for TokenKind {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "NonCollateral" => Ok(Self::NonCollateral),
+            "Collateral" => Ok(Self::Collateral),
+            "Claim" => bail!("cannot set pool token as claim"),
+            s => bail!("invalid token type {s}"),
+        }
+    }
+}
+
+impl Display for TokenKind {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            TokenKind::NonCollateral => write!(f, "NonCollateral"),
+            TokenKind::Collateral => write!(f, "Collateral"),
+            TokenKind::Claim => write!(f, "Claim"),
+        }
+    }
+}
+
+impl PartialEq<jet_metadata::TokenKind> for TokenKind {
+    fn eq(&self, other: &jet_metadata::TokenKind) -> bool {
+        let converted: jet_metadata::TokenKind = (*self).into();
+        converted == *other
+    }
+}
+
+pub async fn process_register_adapter(client: &Client, adapter: Pubkey) -> Result<Plan> {
+    let adapter_md_address = get_metadata_address(&adapter);
+    let ix = ControlIxBuilder::new(resolve_payer(client)?);
+
+    if client.account_exists(&adapter_md_address).await? {
+        println!("address {adapter} is already registered");
+        return Ok(Plan::default());
+    }
+
+    Ok(client
+        .plan()?
+        .instructions(
+            [],
+            [format!("set-adapter {adapter}")],
+            [ix.register_adapter(&adapter)],
+        )
+        .build())
+}
+
+pub async fn process_set_liquidator(
+    client: &Client,
+    liquidator: Pubkey,
+    is_liquidator: bool,
+) -> Result<Plan> {
+    let liquidator_md_address = get_metadata_address(&liquidator);
+    let ix = ControlIxBuilder::new(resolve_payer(client)?);
+
+    let is_currently_liquidator = client.account_exists(&liquidator_md_address).await?;
+    if is_currently_liquidator == is_liquidator {
+        let word = if is_liquidator { "already" } else { "not" };
+        println!("address {liquidator} is {word} a liquidator");
+        return Ok(Plan::default());
+    }
+
+    Ok(client
+        .plan()?
+        .instructions(
+            [],
+            [format!("set-liquidator {liquidator} = {is_liquidator}")],
+            [ix.set_liquidator(&liquidator, is_liquidator)],
+        )
+        .build())
+}
+
+pub async fn process_list_top_accounts(client: &Client, limit: usize) -> Result<Plan> {
+    let all_margin_accounts = client.rpc().get_program_accounts(&jet_margin::ID).await?;
+    let margin_user_account_size = 8 + std::mem::size_of::<MarginAccount>();
+
+    let mut all_user_accounts = all_margin_accounts
+        .into_iter()
+        .filter_map(|(address, account)| {
+            if account.data.len() != margin_user_account_size {
+                return None;
+            }
+
+            match MarginAccount::try_deserialize(&mut &account.data[..]) {
+                Ok(deserialized) => Some((address, deserialized)),
+                Err(_) => {
+                    eprintln!("could not deserialize margin account {address}");
+                    None
+                }
+            }
+        })
+        .collect::<Vec<_>>();
+
+    let refresh_account_tasks = all_user_accounts.iter_mut().map(|(address, account)| {
+        refresh_account_positions(client, account).map(|result| (*address, result))
+    });
+
+    let results = futures::future::join_all(refresh_account_tasks).await;
+    let mut accounts = vec![];
+
+    let mut blacklist_count = 0;
+    for (address, result) in results {
+        if let Err(e) = &result {
+            println!("failed refreshing margin account {address}: {e:?}");
+            continue;
+        }
+
+        if !result.unwrap() {
+            blacklist_count += 1;
+            continue;
+        }
+
+        let account = all_user_accounts
+            .iter()
+            .find(|(addr, _)| *addr == address)
+            .unwrap()
+            .1;
+
+        accounts.push(MarginAccountSummary {
+            address,
+            position_count: account.positions().count(),
+            valuation: account.valuation().unwrap(),
+        });
+    }
+
+    println!("skipped {blacklist_count} blacklisted accounts");
+    println!("found {} margin accounts", accounts.len());
+
+    println!("Top {limit} accounts by risk:");
+    show_top_accounts_by(limit, &mut accounts, compare_account_risk);
+    println!("Top {limit} accounts by equity:");
+    show_top_accounts_by(limit, &mut accounts, compare_account_equity);
+    println!("Top {limit} accounts by collateral:");
+    show_top_accounts_by(limit, &mut accounts, compare_account_weighted_collateral);
+    println!("Top {limit} accounts by required collateral:");
+    show_top_accounts_by(limit, &mut accounts, compare_account_required_collateral);
+
+    Ok(Plan::default())
+}
+
+async fn refresh_account_positions(client: &Client, account: &mut MarginAccount) -> Result<bool> {
+    let position_mints = account.positions().map(|p| p.token).collect::<Vec<_>>();
+
+    for mint in position_mints {
+        let position = account.get_position_mut(&mint).unwrap();
+        let price_info = match position.adapter {
+            adapter if adapter == jet_margin_pool::ID => {
+                let margin_pool_address = client.read_mint(&mint).await?.mint_authority.unwrap();
+                let margin_pool = client
+                    .read_anchor_account::<MarginPool>(&margin_pool_address)
+                    .await?;
+
+                if TOKEN_BLACKLIST.contains(&margin_pool.token_mint) {
+                    return Ok(false);
+                }
+
+                let mut oracle_data = client
+                    .rpc()
+                    .get_account(&margin_pool.token_price_oracle)
+                    .await?;
+                let price_oracle = pyth_sdk_solana::load_price_feed_from_account(
+                    &margin_pool.token_price_oracle,
+                    &mut oracle_data,
+                )?;
+
+                let prices = margin_pool.calculate_prices(&price_oracle)?;
+
+                let price_value = if mint == margin_pool.deposit_note_mint {
+                    prices.deposit_note_price
+                } else {
+                    prices.loan_note_price
+                };
+
+                PriceInfo::new_valid(
+                    price_oracle.expo,
+                    price_value,
+                    SystemTime::now()
+                        .duration_since(UNIX_EPOCH)
+                        .unwrap()
+                        .as_secs(),
+                )
+            }
+
+            adapter => {
+                bail!("unknown position adapter {adapter}");
+            }
+        };
+
+        position.set_price(&price_info).unwrap();
+    }
+
+    Ok(true)
+}
+
+#[derive(Debug, Clone)]
+struct MarginAccountSummary {
+    address: Pubkey,
+    position_count: usize,
+    valuation: Valuation,
+}
+
+impl MarginAccountSummary {
+    fn risk(&self) -> f64 {
+        if self.valuation.effective_collateral.as_u64(0) == 0 {
+            return 0.0;
+        }
+
+        (self.valuation.required_collateral / self.valuation.effective_collateral)
+            .to_string()
+            .parse::<f64>()
+            .unwrap()
+    }
+}
+
+impl From<MarginAccountSummary> for comfy_table::Cells {
+    fn from(summary: MarginAccountSummary) -> Self {
+        Self(vec![
+            summary.address.into(),
+            summary.position_count.into(),
+            summary.valuation.equity.into(),
+            summary.valuation.weighted_collateral.into(),
+            summary.valuation.required_collateral.into(),
+            summary.valuation.effective_collateral.into(),
+            summary.risk().into(),
+        ])
+    }
+}
+
+fn show_top_accounts_by(
+    limit: usize,
+    list: &mut [MarginAccountSummary],
+    comparator: impl Fn(&MarginAccountSummary, &MarginAccountSummary) -> std::cmp::Ordering,
+) {
+    list.sort_by(comparator);
+
+    let rows = list.iter().rev().take(limit).cloned().collect::<Vec<_>>();
+    let mut output_table = Table::new();
+
+    output_table
+        .load_preset(UTF8_FULL)
+        .set_content_arrangement(comfy_table::ContentArrangement::Dynamic)
+        .set_header(vec![
+            "Account",
+            "Position Count",
+            "Equity",
+            "Weighted Collateral",
+            "Required Collateral",
+            "Effective Collateral",
+            "Risk",
+        ]);
+
+    for row in rows {
+        output_table.add_row(row);
+    }
+
+    println!("{output_table}");
+}
+
+fn compare_account_equity(
+    a: &MarginAccountSummary,
+    b: &MarginAccountSummary,
+) -> std::cmp::Ordering {
+    a.valuation.equity.cmp(&b.valuation.equity)
+}
+
+fn compare_account_weighted_collateral(
+    a: &MarginAccountSummary,
+    b: &MarginAccountSummary,
+) -> std::cmp::Ordering {
+    a.valuation
+        .weighted_collateral
+        .cmp(&b.valuation.weighted_collateral)
+}
+
+fn compare_account_required_collateral(
+    a: &MarginAccountSummary,
+    b: &MarginAccountSummary,
+) -> std::cmp::Ordering {
+    a.valuation
+        .required_collateral
+        .cmp(&b.valuation.required_collateral)
+}
+
+fn compare_account_risk(a: &MarginAccountSummary, b: &MarginAccountSummary) -> std::cmp::Ordering {
+    a.risk().partial_cmp(&b.risk()).unwrap()
+}
---
+++ libraries/rust/ctl/src/app_config.rs
@@ -0,0 +1,288 @@
+use std::{
+    collections::HashMap,
+    path::{Path, PathBuf},
+};
+
+use anyhow::{bail, Context, Result};
+use jet_margin_sdk::bonds::BondManager;
+use serde::{Deserialize, Serialize};
+use serde_with::{serde_as, DisplayFromStr};
+
+use solana_sdk::pubkey::Pubkey;
+
+use crate::{
+    client::Client,
+    config::{
+        BondMarketsDefinition, ConfigType, DependenciesDefinition, RpcDefinition,
+        SerumMarketsDefinition, TokenDefinition,
+    },
+    serum::SerumMarketAccount,
+};
+
+#[serde_as]
+#[derive(Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct JetAppConfig {
+    #[serde_as(as = "DisplayFromStr")]
+    pub control_program_id: Pubkey,
+
+    #[serde_as(as = "DisplayFromStr")]
+    pub margin_program_id: Pubkey,
+
+    #[serde_as(as = "DisplayFromStr")]
+    pub margin_pool_program_id: Pubkey,
+
+    #[serde_as(as = "DisplayFromStr")]
+    pub margin_serum_program_id: Pubkey,
+
+    #[serde_as(as = "DisplayFromStr")]
+    pub margin_swap_program_id: Pubkey,
+
+    #[serde_as(as = "DisplayFromStr")]
+    pub metadata_program_id: Pubkey,
+
+    #[serde_as(as = "DisplayFromStr")]
+    pub orca_swap_program_id: Pubkey,
+
+    #[serde_as(as = "DisplayFromStr")]
+    pub serum_program_id: Pubkey,
+
+    #[serde_as(as = "Option<DisplayFromStr>")]
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub faucet_program_id: Option<Pubkey>,
+
+    pub url: String,
+
+    pub tokens: HashMap<String, TokenInfo>,
+    pub markets: HashMap<String, SerumMarketInfo>,
+    pub bond_markets: HashMap<String, BondMarketInfo>,
+}
+
+#[serde_as]
+#[derive(Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct TokenInfo {
+    pub symbol: String,
+    pub name: String,
+    pub decimals: u8,
+    pub precision: u8,
+
+    #[serde_as(as = "Option<DisplayFromStr>")]
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub faucet: Option<Pubkey>,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub faucet_limit: Option<u64>,
+
+    #[serde_as(as = "DisplayFromStr")]
+    pub mint: Pubkey,
+}
+
+#[serde_as]
+#[derive(Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct SerumMarketInfo {
+    pub symbol: String,
+    pub base_symbol: String,
+    pub quote_symbol: String,
+    pub base_decimals: u8,
+    pub quote_decimals: u8,
+
+    #[serde_as(as = "DisplayFromStr")]
+    pub market: Pubkey,
+
+    #[serde(flatten)]
+    pub market_info: SerumMarketAccount,
+}
+
+#[serde_as]
+#[derive(Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct BondMarketInfo {
+    pub symbol: String,
+
+    #[serde_as(as = "DisplayFromStr")]
+    pub bond_manager: Pubkey,
+
+    #[serde(flatten)]
+    pub market_info: BondManager,
+}
+
+impl JetAppConfig {
+    pub async fn generate_from_config_dir(client: &Client, dir: &Path) -> Result<Self> {
+        let rpc = Self::read_rpc_config(dir.join("rpc.toml")).await?;
+        let deps = Self::read_dependency_programs(dir.join("dependencies.toml")).await?;
+        let tokens = Self::generate_token_map_from_dir(client, dir.join("tokens")).await?;
+        let markets = Self::generate_market_map(
+            client,
+            &deps.serum_program_id,
+            dir.join("serum-markets.toml"),
+        )
+        .await?;
+        let bond_markets =
+            Self::generate_bond_market_map(client, dir.join("bond-markets.toml")).await?;
+
+        Ok(Self {
+            control_program_id: jet_margin_sdk::jet_control::ID,
+            margin_program_id: jet_margin_sdk::jet_margin::ID,
+            margin_pool_program_id: jet_margin_sdk::jet_margin_pool::ID,
+            margin_swap_program_id: jet_margin_sdk::jet_margin_swap::ID,
+            metadata_program_id: jet_margin_sdk::jet_metadata::ID,
+            margin_serum_program_id: Pubkey::default(),
+            orca_swap_program_id: deps.orca_program_id,
+            serum_program_id: deps.serum_program_id,
+            faucet_program_id: deps.faucet_program_id,
+            url: rpc.default,
+            tokens,
+            markets,
+            bond_markets,
+        })
+    }
+
+    async fn generate_token_map_from_dir(
+        client: &Client,
+        dir: PathBuf,
+    ) -> Result<HashMap<String, TokenInfo>> {
+        let mut token_infos = HashMap::new();
+        let mut dir_contents = tokio::fs::read_dir(dir).await?;
+
+        while let Some(entry) = dir_contents.next_entry().await? {
+            if !entry.metadata().await?.is_file() {
+                continue;
+            }
+
+            let token_def = Self::read_token_config(entry.path()).await?;
+            let token_mint = client.read_mint(&token_def.config.mint).await?;
+
+            token_infos.insert(
+                token_def.token.symbol.clone(),
+                TokenInfo {
+                    symbol: token_def.token.symbol,
+                    name: token_def.token.name,
+                    mint: token_def.config.mint,
+                    decimals: token_mint.decimals,
+                    precision: token_def.token.precision,
+                    faucet: token_def.token.faucet,
+                    faucet_limit: token_def.token.faucet_limit,
+                },
+            );
+        }
+
+        Ok(token_infos)
+    }
+
+    async fn generate_market_map(
+        client: &Client,
+        serum_program_id: &Pubkey,
+        path: PathBuf,
+    ) -> Result<HashMap<String, SerumMarketInfo>> {
+        let mut markets = HashMap::new();
+        let market_def = Self::read_serum_config(path).await?;
+
+        for market in market_def.markets {
+            let (base_symbol, quote_symbol) = parse_symbol_pair(&market.symbol_pair)?;
+            let market_info =
+                crate::serum::read_market_account(client, serum_program_id, &market.address)
+                    .await?;
+            let base_mint = client.read_mint(&market_info.base_mint).await?;
+            let quote_mint = client.read_mint(&market_info.quote_mint).await?;
+
+            markets.insert(
+                format!("{base_symbol}_{quote_symbol}"),
+                SerumMarketInfo {
+                    symbol: market.symbol_pair,
+                    base_decimals: base_mint.decimals,
+                    quote_decimals: quote_mint.decimals,
+                    base_symbol,
+                    quote_symbol,
+                    market: market.address,
+                    market_info,
+                },
+            );
+        }
+
+        Ok(markets)
+    }
+
+    async fn generate_bond_market_map(
+        client: &Client,
+        path: PathBuf,
+    ) -> Result<HashMap<String, BondMarketInfo>> {
+        let mut bond_markets = HashMap::new();
+        let market_def = Self::read_bonds_config(path).await?;
+
+        for market in market_def.markets {
+            let manager: BondManager = client.read_anchor_account(&market.manager).await?;
+            bond_markets.insert(
+                format!("{}_{}", market.symbol, market.tenor),
+                BondMarketInfo {
+                    symbol: market.symbol.clone(),
+                    bond_manager: market.manager,
+                    market_info: manager,
+                },
+            );
+        }
+
+        Ok(bond_markets)
+    }
+
+    async fn read_bonds_config(path: PathBuf) -> Result<BondMarketsDefinition> {
+        match crate::config::read_config_file(&path)
+            .await
+            .with_context(|| format!("while reading bond market definition at {:?}", &path))?
+        {
+            ConfigType::BondMarkets(market_def) => Ok(market_def),
+            _ => bail!("config {path:?} is not in the right format"),
+        }
+    }
+
+    async fn read_token_config(path: PathBuf) -> Result<TokenDefinition> {
+        match crate::config::read_config_file(&path)
+            .await
+            .with_context(|| format!("while reading token definition at {:?}", &path))?
+        {
+            ConfigType::Token(token_def) => Ok(token_def),
+            _ => bail!("config {path:?} is not in the right format"),
+        }
+    }
+
+    async fn read_serum_config(path: PathBuf) -> Result<SerumMarketsDefinition> {
+        match crate::config::read_config_file(&path)
+            .await
+            .with_context(|| format!("while reading serum market definition at {:?}", &path))?
+        {
+            ConfigType::SerumMarkets(market_def) => Ok(market_def),
+            _ => bail!("config {path:?} is not in the right format"),
+        }
+    }
+
+    async fn read_rpc_config(path: PathBuf) -> Result<RpcDefinition> {
+        match crate::config::read_config_file(&path)
+            .await
+            .with_context(|| format!("while reading rpc definition at {:?}", &path))?
+        {
+            ConfigType::Rpc(rpc_def) => Ok(rpc_def),
+            _ => bail!("config {path:?} is not in the right format"),
+        }
+    }
+
+    async fn read_dependency_programs(path: PathBuf) -> Result<DependenciesDefinition> {
+        match crate::config::read_config_file(&path)
+            .await
+            .with_context(|| format!("while reading dependency definition at {:?}", &path))?
+        {
+            ConfigType::Dependencies(deps) => Ok(deps),
+            _ => bail!("config {path:?} is not in the right format"),
+        }
+    }
+}
+
+fn parse_symbol_pair(pair: &str) -> Result<(String, String)> {
+    let mut symbol_elements = pair.split('/');
+    let (base, quote) = (symbol_elements.next(), symbol_elements.next());
+
+    match (base, quote) {
+        (Some(base), Some(quote)) => Ok((base.to_owned(), quote.to_owned())),
+        _ => bail!("invalid serum-market symbol format: {pair}"),
+    }
+}
