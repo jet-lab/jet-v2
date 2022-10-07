2af2b30b807302383344f6c2758c549677c6c6b6
qiqicodes <68657634+qiqicodes@users.noreply.github.com>
2022-10-07 14:34:56 -0700

    adds error flows to e2e tests

--- app/cypress/e2e/spec.cy.ts
+++ app/cypress/e2e/spec.cy.ts
@@ -30,16 +30,65 @@ describe('Main Flows', () => {
     withdraw('SOL', 0.3);
   });
 
-    it('Borrow and repay SOL from wallet', () => {
-      airdrop('SOL', 'SOL');
-      deposit('SOL', 0.5);
-      borrow('SOL', 0.3);
-      repay('SOL', 0.3, false);
+  it('Borrow and repay SOL from wallet', () => {
+    airdrop('SOL', 'SOL');
+    deposit('SOL', 0.5);
+    borrow('SOL', 0.3);
+    repay('SOL', 0.3, false);
+  });
+
+  it('Borrow and repay SOL from existing margin account', () => {
+    deposit('SOL', 0.5);
+    borrow('SOL', 0.3, true);
+    repay('SOL', 0.3, true);
+  });
+});
+
+describe('Error Flows', () => {
+  it('Connects a new test wallet and creates an account', () => {
+    cy.on('uncaught:exception', (err, runnable, promise) => {
+      console.log('***** ERROR ****');
+      if (promise) {
+        return false;
+      }
     });
 
-    it('Borrow and repay SOL from existing margin account', () => {
-      deposit('SOL', 0.5);
-      borrow('SOL', 0.3, true);
-      repay('SOL', 0.3, true);
-    });
+    cy.clearLocalStorage();
+    loadPageAndCreateAccount();
+    cy.wait(8000);
+  });
+
+  it('All lend and borrow transactions should be disabled, because SOL in wallet is under fees buffer amount', () => {
+    const disabledInput = () => {
+      cy.get('.ant-modal-content input.ant-input', { timeout: 30000 }).should('be.disabled');
+    };
+    const notEnoughSolMessage = () => {
+      cy.contains("You don't have enough SOL in your wallet to cover transaction fees.", { timeout: 10000 });
+    };
+    const closeModal = () => {
+      cy.get('button.ant-modal-close', { timeout: 1000 }).click();
+    };
+
+    deposit('SOL', 0.9);
+    cy.get('.SOL-pools-table-row', { timeout: 30000 }).click();
+    cy.get('button').contains('Deposit', { timeout: 1000 }).click();
+    cy.contains('100%', { timeout: 30000 }).click();
+    notEnoughSolMessage();
+    closeModal();
+
+    cy.get('button').contains('Withdraw', { timeout: 1000 }).click();
+    disabledInput();
+    notEnoughSolMessage();
+    closeModal();
+
+    cy.get('button').contains('Borrow', { timeout: 1000 }).click();
+    disabledInput();
+    notEnoughSolMessage();
+    closeModal();
+
+    cy.get('button').contains('Repay', { timeout: 1000 }).click();
+    disabledInput();
+    notEnoughSolMessage();
+    closeModal();
+  });
 });
