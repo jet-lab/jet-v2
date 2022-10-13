e9ce9545156b04c5a7f5f46b14c40bfbea75353d
Drew Nutter <dnut@users.noreply.github.com>
2022-10-13 10:30:49 -0400

    docs

--- libraries/rust/proc-macros/src/lib.rs
+++ libraries/rust/proc-macros/src/lib.rs
@@ -2,6 +2,60 @@ use proc_macro::TokenStream;
 use quote::quote;
 use syn::{self, DeriveInput};
 
+/// Implements both traits BondManagerProvider and TokenProgramProvider. By
+/// default, this expects fields named token_program and bond_manager with the
+/// appropriate types. If either field is missing, then the data must be nested.
+/// Annotate the nesting field with attribute #[token_manager] if that field
+/// contains a token_manager. If further nested, and the field contains a
+/// sub-field called "subfield" which contains the token_manager, mark the field
+/// with #[token_manager(subfield)]. If token_manager is another level deep
+/// within "subsubfield", use #[token_manager(subfield::subsubfield)]. There is
+/// no limit to the accessible depth.
+/// 
+/// Examples
+/// 
+/// ```ignore
+/// #[derive(BondTokenManager)]
+/// struct BaseCase<'info> {
+///     bond_manager: AccountLoader<'info, BondManager>,
+///     token_program: Program<'info, Token>,
+/// }
+/// ```
+/// 
+/// ```ignore
+/// #[derive(BondTokenManager)]
+/// struct Top<'info> {
+///     #[bond_manager]
+///     nested: Bottom<'info>,
+///     token_program: Program<'info, Token>,
+/// }
+/// 
+/// struct Bottom<'info> {
+///     bond_manager: AccountLoader<'info, BondManager>,
+/// }
+/// ```
+/// 
+/// ```ignore
+/// #[derive(BondTokenManager)]
+/// struct Top<'info> {
+///     #[bond_manager(mid_two)]
+///     #[token_program(mid_two::bottom)]
+///     mid_one: MiddleOne<'info>,
+/// }
+/// 
+/// struct MiddleOne<'info> {
+///     mid_two: MiddleTwo<'info>,
+/// }
+/// 
+/// struct MiddleTwo<'info> {
+///     bond_manager: AccountLoader<'info, BondManager>,
+///     bottom: Bottom<'info>,
+/// }
+/// 
+/// struct Bottom<'info> {
+///     token_program: Program<'info, Token>,
+/// }
+/// ```
 #[proc_macro_derive(BondTokenManager, attributes(bond_manager, token_program))]
 pub fn bond_token_manager_derive(input: TokenStream) -> TokenStream {
     let ast = syn::parse(input).unwrap();
@@ -36,6 +90,29 @@ fn impl_token_program_provider(ast: &DeriveInput) -> quote::__private::TokenStre
     }
 }
 
+/// If `ast` is a struct, this will find the first field with an attribute named
+/// `attr_name`, and return a TokenStream representing the code that you would
+/// use to access a field with the name `attr_name` within the annotated field
+/// of this struct. It will also include any path passed as an argument as
+/// intermediary fields.
+/// 
+/// ```ignore
+/// /// Searching this struct for "thing" returns ".bar.thing"
+/// struct Foo {
+///     #[thing]
+///     bar: Bar,
+/// }
+/// ```
+/// 
+/// ```ignore
+/// /// Searching this struct for "thing" returns ".bar.some.nesting.thing"
+/// struct Foo {
+///     #[thing(some::nesting)]
+///     bar: Bar,
+/// }
+/// ```
+/// 
+/// Returns None if not a struct or there is no field with the attribute.
 fn find_attr_path_as_accessor(
     ast: &DeriveInput,
     attr_name: &str,
