00841d1a02da9a9a189370569247ebd3b929df8c
Davide Scalzo <scalzodavide@yahoo.it>
2022-10-17 11:17:24 +0100

    uodate frontend to use localnet config file

--- app/src/state/StateSyncer.tsx
+++ app/src/state/StateSyncer.tsx
@@ -1,18 +1,17 @@
-import { useMarginConfigSyncer } from './config/marginConfig';
+import { useMainConfigSyncer } from './config/marginConfig';
 import { usePoolsSyncer } from './pools/pools';
 import { useWalletTokensSyncer } from './user/walletTokens';
 import { useAccountsSyncer } from './user/accounts';
 import { useSplSwapSyncer } from './swap/splSwap';
-// import { useFixedTermSync } from './fixed/fixed-term-market-sync';
+import { useFixedTermSync } from './fixed/fixed-term-market-sync';
 
-// React component to utilize all sync hooks from bigger state
 export function StateSyncer(): JSX.Element {
-  useMarginConfigSyncer();
+  useMainConfigSyncer();
   usePoolsSyncer();
   useWalletTokensSyncer();
   useAccountsSyncer();
   useSplSwapSyncer();
-  // useFixedTermSync();
+  useFixedTermSync();
 
   return <></>;
 }
