54739f7931bc273fa2ca3a91cee308f5b7a696c0
Davide Scalzo <scalzodavide@yahoo.it>
2022-10-17 16:42:53 +0100

    call lendOrder ix

--- libraries/ts/bonds/src/bondMarket.ts
+++ libraries/ts/bonds/src/bondMarket.ts
@@ -10,9 +10,7 @@ import { order_id_to_string, rate_to_price } from "./wasm-utils"
 export const OrderSideBorrow = { borrow: {} }
 export const OrderSideLend = { lend: {} }
 export type OrderSide = typeof OrderSideBorrow | typeof OrderSideLend
-
 export const U64_MAX = 18_446_744_073_709_551_615n
-
 export interface OrderParams {
   maxBondTicketQty: BN
   maxUnderlyingTokenQty: BN
@@ -22,7 +20,6 @@ export interface OrderParams {
   postAllowed: boolean
   autoStake: boolean
 }
-
 /**
  * The raw struct as found on chain
  */
@@ -48,7 +45,6 @@ export interface BondManagerInfo {
   duration: BN
   nonce: BN
 }
-
 /** MarginUser account as found on-chain */
 export interface MarginUserInfo {
   version: BN
@@ -61,7 +57,6 @@ export interface MarginUserInfo {
   debt: DebtInfo
   assets: AssetInfo
 }
-
 export interface DebtInfo {
   nextNewObligationSeqNo: BN
   nextUnpaidObligationSeqNo: BN
@@ -69,20 +64,17 @@ export interface DebtInfo {
   pending: BN
   committed: BN
 }
-
 export interface AssetInfo {
   entitledTokens: BN
   entitledTickets: BN
   _reserved0: number[]
 }
-
 export interface ClaimTicket {
   owner: PublicKey
   bondManager: PublicKey
   maturationTimestamp: BN
   redeemable: BN
 }
-
 /**
  * Class for loading and interacting with a BondMarket
  */
@@ -101,33 +93,32 @@ export class BondMarket {
     collateralMint: PublicKey
     underlyingOracle: PublicKey
     ticketOracle: PublicKey
+    marginAdapterMetadata: PublicKey
   }
   readonly info: BondManagerInfo
   readonly program: Program<JetBonds>
-
   private constructor(
     bondManager: PublicKey,
     claimsMetadata: PublicKey,
+    marginAdapterMetadata: PublicKey,
     program: Program<JetBonds>,
     info: BondManagerInfo
   ) {
     this.addresses = {
       ...info,
       claimsMetadata,
+      marginAdapterMetadata,
       bondManager
     }
     this.program = program
     this.info = info
   }
-
   get address() {
     return this.addresses.bondManager
   }
-
   get provider() {
     return this.program.provider
   }
-
   /**
    * Loads the program state from on chain and returns a `BondMarket` client
    * class for interaction with the market
@@ -144,10 +135,15 @@ export class BondMarket {
     let data = await fetchData(program.provider.connection, bondManager)
     let info: BondManagerInfo = program.coder.accounts.decode("BondManager", data)
     const claimsMetadata = await findDerivedAccount([info.claimsMint], new PublicKey(jetMetadataProgramId))
-
-    return new BondMarket(new PublicKey(bondManager), new PublicKey(claimsMetadata), program, info)
+    const marginAdapterMetadata = await findDerivedAccount([program.programId], new PublicKey(jetMetadataProgramId))
+    return new BondMarket(
+      new PublicKey(bondManager),
+      new PublicKey(claimsMetadata),
+      new PublicKey(marginAdapterMetadata),
+      program,
+      info
+    )
   }
-
   async requestBorrowIx(
     user: MarginAccount,
     payer: Address,
@@ -194,7 +190,6 @@ export class BondMarket {
     const borrowerAccount = await this.deriveMarginUserAddress(user)
     const obligation = await this.deriveObligationAddress(borrowerAccount, seed)
     const claims = await this.deriveMarginUserClaims(borrowerAccount)
-
     return this.program.methods
       .marginBorrowOrder(params, Buffer.from(seed))
       .accounts({
@@ -209,7 +204,6 @@ export class BondMarket {
       })
       .instruction()
   }
-
   async offerLoanIx(
     user: MarginAccount,
     amount: BN,
@@ -231,7 +225,6 @@ export class BondMarket {
     }
     return await this.lendIx(user.address, userTicketVault, userTokenVault, payer, params, seed)
   }
-
   async lendNowIx(user: MarginAccount, amount: BN, payer: Address, seed: Uint8Array): Promise<TransactionInstruction> {
     const userTokenVault = await getAssociatedTokenAddress(this.addresses.underlyingTokenMint, user.address, true)
     const userTicketVault = await getAssociatedTokenAddress(this.addresses.bondTicketMint, user.address, true)
@@ -244,10 +237,8 @@ export class BondMarket {
       postAllowed: false,
       autoStake: true
     }
-
     return await this.lendIx(user.address, userTicketVault, userTokenVault, payer, params, seed)
   }
-
   async lendIx(
     user: Address,
     userTicketVault: Address,
@@ -271,10 +262,8 @@ export class BondMarket {
       })
       .instruction()
   }
-
   async cancelOrderIx(user: MarginAccount, orderId: Uint8Array, side: OrderSide): Promise<TransactionInstruction> {
     const bnOrderId = new BN(order_id_to_string(orderId))
-
     return await this.program.methods
       .cancelOrder(bnOrderId)
       .accounts({
@@ -283,17 +272,13 @@ export class BondMarket {
       })
       .instruction()
   }
-
   async registerAccountWithMarket(user: MarginAccount, payer: Address): Promise<TransactionInstruction> {
     const borrowerAccount = await this.deriveMarginUserAddress(user)
     const claims = await this.deriveMarginUserClaims(borrowerAccount)
     const collateral = await this.deriveMarginUserCollateral(borrowerAccount)
-
     const underlyingSettlement = await getAssociatedTokenAddress(this.addresses.underlyingTokenMint, user.address, true)
     const ticketSettlement = await getAssociatedTokenAddress(this.addresses.bondTicketMint, user.address, true)
-
     // await user.getOrRegisterPosition(this.addresses.claimsMint)
-
     return await this.program.methods
       .initializeMarginUser()
       .accounts({
@@ -311,7 +296,6 @@ export class BondMarket {
       })
       .instruction()
   }
-
   /**
    *
    * @param user the margin account to refresh
@@ -330,41 +314,32 @@ export class BondMarket {
       })
       .instruction()
   }
-
   async deriveMarginUserAddress(user: MarginAccount): Promise<PublicKey> {
     return await findDerivedAccount(["margin_borrower", this.address, user.address], this.program.programId)
   }
-
   async deriveMarginUserClaims(borrowerAccount: Address): Promise<PublicKey> {
     return await findDerivedAccount(["user_claims", borrowerAccount], this.program.programId)
   }
-
   async deriveMarginUserCollateral(borrowerAccount: Address): Promise<PublicKey> {
     return await findDerivedAccount(["deposit_notes", borrowerAccount], this.program.programId)
   }
-
   async deriveObligationAddress(borrowerAccount: Address, seed: Uint8Array): Promise<PublicKey> {
     return await findDerivedAccount(["obligation", borrowerAccount, seed], this.program.programId)
   }
-
   async deriveClaimTicketKey(ticketHolder: Address, seed: Uint8Array): Promise<PublicKey> {
     return await findDerivedAccount(
       ["claim_ticket", this.address, new PublicKey(ticketHolder), seed],
       this.program.programId
     )
   }
-
   async deriveSplitTicket(user: Address, seed: Uint8Array): Promise<PublicKey> {
     return await findDerivedAccount(["split_ticket", user, seed], this.program.programId)
   }
-
   async fetchOrderbook(): Promise<Orderbook> {
     return await Orderbook.load(this)
   }
-
   async fetchMarginUser(user: MarginAccount): Promise<MarginUserInfo> {
     let data = (await this.provider.connection.getAccountInfo(await this.deriveMarginUserAddress(user)))!.data
-
     return await this.program.coder.accounts.decode("MarginUser", data)
   }
 }
