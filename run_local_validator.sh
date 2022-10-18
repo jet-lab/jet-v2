5e0d9091d21215e322752b5678435d7df6114413
suscd <dev@suscd.io>
2022-10-18 08:16:23 -0700

    misc fixes for localnet (#386)
    - `run_local_validator.sh` script now resumes by default, pass `-r` or
      `--reset` to reset the state
    - Fix pool flags not passing through during environment setup
    - Fix API for `withAdapterInvoke` to not require redundant accounts

--- run_local_validator.sh
+++ run_local_validator.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-exec ./tests/scripts/on_localnet.sh start-new-validator
+case $1 in
+    -r|--reset)
+        exec ./tests/scripts/on_localnet.sh start-new-validator
+    ;;
+esac
+
+exec ./tests/scripts/on_localnet.sh resume-validator
