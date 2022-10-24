d6447afde884b055a7bbc9061c5337ca3048823b
zachanon <zachanon@gmail.com>
2022-10-24 23:59:56 +0700

    working anchor test

--- tests/scripts/anchor.sh
+++ tests/scripts/anchor.sh
@@ -6,8 +6,11 @@ if [[ ${SOLANA_LOGS:-false} == true ]]; then
 	solana -ul logs &
 fi
 
-# initialize some state on chain to test against
-RUST_BACKTRACE=1 cargo run --package hosted-tests --bin launch_bonds
+cargo run --bin jetctl -- test init-env -ul --no-confirm localnet.toml
+cargo run --bin jetctl -- test generate-app-config -ul --no-confirm localnet.toml -o app/public/localnet.config.json
+cargo run --bin jet-oracle-mirror -- -s $SOLANA_MAINNET_RPC -tl &
+sleep 5
+
 
 # run the typescript tests
 npx ts-mocha -p ./tests/tsconfig.json -t 1000000 --paths 'tests/**/*.test.ts'
