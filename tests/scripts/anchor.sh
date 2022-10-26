441d469ecad5f7d8c1f70655815b211b859ddf25
suscd <me@suscd.io>
2022-10-25 20:50:38 -0700

    synchronize oracle startup with ts test start

--- tests/scripts/anchor.sh
+++ tests/scripts/anchor.sh
@@ -9,8 +9,18 @@ fi
 cargo run --bin jetctl -- test init-env -ul --no-confirm localnet.toml
 cargo run --bin jetctl -- test generate-app-config -ul --no-confirm localnet.toml -o app/public/localnet.config.json
 cargo run --bin jet-oracle-mirror -- -s $SOLANA_MAINNET_RPC -tl &
-sleep 5
 
+echo "waiting for oracles ..."
+
+while true; do
+	if [[ -f tests/oracle-mirror.pid ]]; then
+		break;
+	fi
+
+	sleep 5
+done
+
+echo "oracles ready!"
 
 # run the typescript tests
 npx ts-mocha -p ./tests/tsconfig.json -t 1000000 --paths 'tests/**/*.test.ts'
