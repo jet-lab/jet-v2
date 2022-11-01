fa5b99e3fd005efc54a3619902bf6294825e40c2
Drew Nutter <dnut@users.noreply.github.com>
2022-11-01 12:50:05 -0400

    merge fix

--- libraries/ts/bonds/src/bondMarket.ts
+++ libraries/ts/bonds/src/bondMarket.ts
@@ -194,7 +194,7 @@ export class BondMarket {
     const params: OrderParams = {
       maxBondTicketQty: new BN(U64_MAX.toString()),
       maxUnderlyingTokenQty: amount,
-      limitPrice: new BN(rate_to_price(BigInt("99999"), BigInt(this.info.duration.toString())).toString()),
+      limitPrice: new BN(rate_to_price(BigInt("99999"), BigInt(this.info.lendDuration.toString())).toString()),
       matchLimit: new BN(U64_MAX.toString()),
       postOnly: false,
       postAllowed: false,
