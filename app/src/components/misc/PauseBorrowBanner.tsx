dac2b13ef1e01ee949ca2205c6f97fc8dc762f27
qiqicodes <68657634+qiqicodes@users.noreply.github.com>
2022-11-16 13:06:17 -0800

    modify banner message

--- app/src/components/misc/PauseBorrowBanner.tsx
+++ app/src/components/misc/PauseBorrowBanner.tsx
@@ -3,7 +3,7 @@ import { Alert } from 'antd';
 // Banner to show user that borrows are temporarily paused
 export function PauseBorrowBanner(): JSX.Element {
   function getMessage() {
-    let message = 'Borrows have been re-enabled, thank you for your patience';
+    let message = 'Borrows have been re-enabled, thank you for your patience.';
     return message;
   }
 
