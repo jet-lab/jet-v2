bb4bca2ef282ad17adcfacacb8c08328a40aaa6b
zachanon <38860696+zachanon@users.noreply.github.com>
2022-12-13 17:34:22 +0900

    Fix key logging error for Consume Events (#499)
    * key fix
    
    * try and move things off the stack
    
    * clippy
    
    * constrain the number of events per transaction
    
    * prune code for coverage

--- programs/fixed-term/src/orderbook/instructions/consume_events/accounts.rs
+++ programs/fixed-term/src/orderbook/instructions/consume_events/accounts.rs
@@ -1,4 +1,4 @@
-use std::convert::TryInto;
+use std::convert::TryFrom;
 
 use anchor_lang::prelude::*;
 use anchor_spl::token::Token;
@@ -108,21 +108,12 @@ impl<'info> UserAccount<'info> {
         Self(account)
     }
 
-    pub fn pubkey(&self) -> Pubkey {
-        self.0.key()
-    }
-
     /// token account that will receive a deposit of underlying or tickets
-    pub fn as_token_account(&self) -> AccountInfo<'info> {
-        self.0.clone()
+    pub fn as_token_account(&self) -> &AccountInfo<'info> {
+        &self.0
     }
 
-    /// arbitrary unchecked account that will be granted ownership of a split ticket
-    pub fn as_owner(&self) -> AccountInfo<'info> {
-        self.0.clone()
-    }
-
-    pub fn margin_user(&self) -> Result<AnchorAccount<'info, MarginUser, Mut>> {
-        self.0.clone().try_into()
+    pub fn margin_user(self) -> Result<Box<AnchorAccount<'info, MarginUser, Mut>>> {
+        Ok(Box::new(AnchorAccount::try_from(self.0)?))
     }
 }
