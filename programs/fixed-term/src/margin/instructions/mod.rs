7af4ed17663031d3c77f31fff52edd721aded725
Drew Nutter <dnut@users.noreply.github.com>
2022-12-16 08:48:49 -0500

    remove new instructions to declutter the name changes, they can go in a later pr

--- programs/fixed-term/src/margin/instructions/mod.rs
+++ programs/fixed-term/src/margin/instructions/mod.rs
@@ -1,21 +1,17 @@
-pub mod close_position;
 pub mod initialize_margin_user;
 pub mod margin_borrow_order;
 pub mod margin_lend_order;
 pub mod margin_redeem_ticket;
 pub mod margin_sell_tickets_order;
 pub mod refresh_position;
-pub mod register_position;
 pub mod repay;
 pub mod settle;
 
-pub use close_position::*;
 pub use initialize_margin_user::*;
 pub use margin_borrow_order::*;
 pub use margin_lend_order::*;
 pub use margin_redeem_ticket::*;
 pub use margin_sell_tickets_order::*;
 pub use refresh_position::*;
-pub use register_position::*;
 pub use repay::*;
 pub use settle::*;
