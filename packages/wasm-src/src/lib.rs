b3e0ac8ee867837b4d5a6c76f1b981c0d3b74006
eyevzz <norman@jetprotocol.io>
2022-12-22 10:54:10 +0000

    Adds console_log!, sets panic hook
    Weirdly, init_module does not appear to get called on load. Leaving
    that to be figured out later.

--- packages/wasm-src/src/lib.rs
+++ packages/wasm-src/src/lib.rs
@@ -1,10 +1,22 @@
-#![allow(non_snake_case)]
-
 pub mod orderbook;
 
 use wasm_bindgen::prelude::*;
 
+/// Initialise the wasm module. Idempotent.
+/// NOTE FIXME despite the start attribute this does not appear to
+/// be called on module import.
+#[wasm_bindgen(start, js_name = initModule)]
+pub fn init_module() {
+    console_error_panic_hook::set_once();
+}
+
 #[wasm_bindgen]
-pub fn MAX_U64() -> u64 {
-    u64::MAX
+extern "C" {
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(s: &str);
+}
+
+#[macro_export]
+macro_rules! console_log {
+    ($($t:tt)*) => ($crate::log(&format_args!($($t)*).to_string()))
 }
