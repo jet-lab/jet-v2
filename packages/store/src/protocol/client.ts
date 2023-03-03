6f55452d699e47b10bc208317926970d8b5cff74
Davide <scalzodavide@yahoo.it>
2023-03-03 16:14:44 +0000

    remove more instances of wasm sdk

--- packages/store/src/protocol/client.ts
+++ packages/store/src/protocol/client.ts
@@ -1,33 +1,34 @@
-import { Connection } from '@solana/web3.js';
-import { JetWebClient, initModule, MarginWebClient, MarginAccountWebClient } from '@jet-lab/jet-client-web';
-import { useJetStore } from '../store';
-import { SolanaConnectionAdapter } from './connection-adapter';
-import { WalletContextState } from '@solana/wallet-adapter-react';
+// import { Connection } from '@solana/web3.js';
+// import { JetWebClient, initModule, MarginWebClient, MarginAccountWebClient } from '@jet-lab/jet-client-web';
+// import { useJetStore } from '../store';
+// import { SolanaConnectionAdapter } from './connection-adapter';
+// import { WalletContextState } from '@solana/wallet-adapter-react';
 
-initModule();
+// initModule();
 
-interface ExtendedMargin extends MarginWebClient {
-  accounts: () => MarginAccountWebClient[];
-}
-interface ExtendedJetClient extends JetWebClient {
-  margin: () => ExtendedMargin;
-}
+// interface ExtendedMargin extends MarginWebClient {
+//   accounts: () => MarginAccountWebClient[];
+// }
+// interface ExtendedJetClient extends JetWebClient {
+//   margin: () => ExtendedMargin;
+// }
 
-export let jetClient: ExtendedJetClient | undefined;
+// export let jetClient: ExtendedJetClient | undefined;
 
-export const initJetClient = async (wallet: WalletContextState) => {
-  console.log('initializing client for ', wallet.publicKey);
-  const { rpc, cluster } = useJetStore.getState().settings;
-  const endpoint = rpc[cluster];
+// export const initJetClient = async (wallet: WalletContextState) => {
+//   console.log('initializing client for ', wallet.publicKey);
+//   const { rpc, cluster } = useJetStore.getState().settings;
+//   const endpoint = rpc[cluster];
 
-  const connection = new Connection(endpoint, 'recent');
-  const adapter = new SolanaConnectionAdapter(wallet, connection);
-  jetClient =
-    adapter.userAddress &&
-    (await JetWebClient.connect(adapter.userAddress, adapter, cluster === 'devnet' ? 'devnet0' : 'default'));
+//   const connection = new Connection(endpoint, 'recent');
+//   const adapter = new SolanaConnectionAdapter(wallet, connection);
+//   jetClient =
+//     adapter.userAddress &&
+//     (await JetWebClient.connect(adapter.userAddress, adapter, cluster === 'devnet' ? 'devnet0' : 'default'));
 
-  await jetClient?.state().syncAccounts();
+//   await jetClient?.state().syncAccounts();
 
-  const accounts = jetClient?.margin().accounts();
-  console.log(accounts);
-};
+//   const accounts = jetClient?.margin().accounts();
+//   console.log(accounts);
+// };
+export {}
