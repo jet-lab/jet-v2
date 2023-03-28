0e6be685a2e476c43d6cb656ef1ac21c9de4ca5d
Neville Dipale <nevilledips@gmail.com>
2023-03-28 15:02:58 +0000

    Merge marginacconuts with accounts

--- packages/store/src/store.ts
+++ packages/store/src/store.ts
@@ -6,10 +6,9 @@ import { createPoolsSlice, type PoolsSlice } from './slices/pools';
 import { createPricesSlice, PricesSlice } from './slices/prices';
 import { createSettingsSlice, SettingsSlice } from './slices/settings';
 import { AccountsSlice, createAccountsSlice } from './slices/accounts';
-import { createMarginAccountsSlice, MarginAccountsSlice } from './slices/marginAccounts';
 import { initWebsocket } from './websocket';
 
-export type JetStore = PoolsSlice & MarketsSlice & PricesSlice & SettingsSlice & AccountsSlice & MarginAccountsSlice;
+export type JetStore = PoolsSlice & MarketsSlice & PricesSlice & SettingsSlice & AccountsSlice;
 
 export const useJetStore = create<JetStore, [['zustand/devtools', never], ['zustand/persist', JetStore]]>(
   devtools(
@@ -20,7 +19,6 @@ export const useJetStore = create<JetStore, [['zustand/devtools', never], ['zust
         ...createPricesSlice(...a),
         ...createSettingsSlice(...a),
         ...createAccountsSlice(...a),
-        ...createMarginAccountsSlice(...a),
       }),
       {
         name: 'jet-state',
