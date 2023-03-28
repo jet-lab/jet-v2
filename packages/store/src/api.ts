5aa0e60e9ae2d83346f4068a14f85330ff390e87
Davide Scalzo <scalzodavide@yahoo.it>
2023-03-28 16:12:18 +0100

    add fixed term action icons for account history (#652)
    * add fixed term action icons for account history
    
    * cleanup store
    
    * fix snapshot call
    
    * revert docker compose changes

--- packages/store/src/api.ts
+++ packages/store/src/api.ts
@@ -1,35 +1,33 @@
-import { MarginAccount, OrderbookSnapshot } from '@jet-lab/margin';
-import { FixedTermMarket } from '@jet-lab/margin';
-import { OpenOrders, OpenPositions } from './types';
+import { OpenOrders, OpenPositions, OrderbookSnapshot } from './types';
 import useSWR, { SWRResponse } from 'swr';
 
-export const useOrdersForUser = (apiEndpoint: string, market?: FixedTermMarket, account?: MarginAccount): SWRResponse<OpenOrders> => {
-  const path = `fixed/open-orders/${market?.address}/${account?.address}`;
+export const useOrdersForUser = (apiEndpoint: string, market?: string, account?: string): SWRResponse<OpenOrders> => {
+  const path = `fixed/open-orders/${market}/${account}`;
   return useSWR<OpenOrders>(path, async () => {
     if (account && market) {
       return fetch(`${apiEndpoint}/${path}`).then(r => r.json());
     } else {
-      return null;
+      return [];
     }
   }, { refreshInterval: 30_000 });
 };
 
-export const useOpenPositions = (apiEndpoint: string, market?: FixedTermMarket, account?: MarginAccount): SWRResponse<OpenPositions> => {
-  const path = `fixed/open-positions/${market?.address}/${account?.address}`;
+export const useOpenPositions = (apiEndpoint: string, market?: string, account?: string): SWRResponse<OpenPositions> => {
+  const path = `fixed/open-positions/${market}/${account}`;
   return useSWR<OpenPositions>(path, async () => {
     if (account && market) {
       return fetch(`${apiEndpoint}/${path}`).then(r => r.json());
     } else {
-      return null;
+      return [];
     }
   }, { refreshInterval: 30_000 });
 };
 
 export const getOrderbookSnapshot = async (
   apiEndpoint: string,
-  market: FixedTermMarket
+  market: string
 ): Promise<OrderbookSnapshot> => {
-  const path = `fixed/orderbook-snapshot/${market.address}`;
+  const path = `fixed/orderbook-snapshot/${market}`;
   const data = await fetch(`${apiEndpoint}/${path}`).then(r => r.json())
   return data
 };
