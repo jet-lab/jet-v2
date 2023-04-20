f107ebc94ca0f7d9620bde7431e3c5412a024f38
Drew Nutter <dnut@users.noreply.github.com>
2023-04-20 14:37:17 -0300

    logging improvements: (#676)
    - debug_msg macro for programs to log in tests when feature is enabled (currently only used in fixed term repay)
    - more granular logging targets in simulation to enable filtering

---
+++ libraries/rust/simulation/src/log.rs
@@ -0,0 +1,32 @@
+/// Declare a custom logging target hierarchy, where the modules may use
+/// different names from the log targets.
+macro_rules! declare_logging {
+    ($parent_mod:ident = $parent_target:literal {
+        $($mod:ident = $target:literal);+$(;)?
+    }) => {
+        mod $parent_mod {
+            $(pub mod $mod {
+                use super::super::declare_logging;
+                declare_logging!(target: concat!($parent_target, "::", $target));
+            })+
+        }
+    };
+    (target: $target:expr) => {
+        declare_logging!(($) trace as trace, $target);
+        declare_logging!(($) debug as debug, $target);
+        declare_logging!(($) info as info, $target);
+        declare_logging!(($) _warn as warn, $target);
+        declare_logging!(($) error as error, $target);
+    };
+    (($dollar:tt) $internal_name:ident as $log_macro:ident, $target:expr) => {
+        #[allow(unused_macros)]
+        macro_rules! $internal_name {
+            ($dollar($arg:tt)+) => {
+                ::log::$log_macro!(target: $target, $dollar($arg)*)
+            };
+        }
+        #[allow(unused_imports)]
+        pub(crate) use $internal_name as $log_macro;
+    };
+}
+pub(crate) use declare_logging;
