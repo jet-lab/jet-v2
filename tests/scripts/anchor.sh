a4bce6e0887ca7d3f0262452289f672d65c21a78
Neville Dipale <nevilledips@gmail.com>
2023-05-02 16:51:18 +0000

    Address review

--- tests/scripts/anchor.sh
+++ tests/scripts/anchor.sh
@@ -9,7 +9,7 @@ fi
 cargo run --bin jetctl -- apply -ul --no-confirm config/localnet/
 solana-keygen new --no-bip39-passphrase -o apps/react-app/public/lookup-authority.json --force
 solana airdrop -k apps/react-app/public/lookup-authority.json -ul 5
-cargo run --bin jetctl -- generate-app-config -ul --no-confirm config/localnet -o apps/react-app/public/localnet.config.json --default-lookup-keypair apps/react-app/public/lookup-authority.json
+cargo run --bin jetctl -- generate-app-config -ul --no-confirm config/localnet/ -o apps/react-app/public/localnet.config.json --override-lookup-authority $(solana-keygen pubkey apps/react-app/public/lookup-authority.json)
 cargo run --bin jet-alt-registry-client -- create-registry -ul --no-confirm --authority-path apps/react-app/public/lookup-authority.json -k apps/react-app/public/lookup-authority.json
 cargo run --bin jet-alt-registry-client -- update-registry -ul --no-confirm --authority-path apps/react-app/public/lookup-authority.json -k apps/react-app/public/lookup-authority.json --airspace-name default
 cargo run --bin jet-oracle-mirror -- -s ${SOLANA_MAINNET_RPC:='https://solana-api.projectserum.com'} -tl &
