8c5d7e7f36a1b42619d8a814b1c54414c0ce718b
Neville Dipale <nevilledips@gmail.com>
2023-05-02 18:29:03 +0000

    fetch localnet lookup authority when starting up

--- apps/react-app/src/state/config/marginConfig.ts
+++ apps/react-app/src/state/config/marginConfig.ts
@@ -12,7 +12,10 @@ export const MainConfig = atom<MarginConfig | undefined>({
 
 // A syncer to be called so that we can have dependent atom state
 export function useMainConfigSyncer() {
-  const cluster = useJetStore(state => state.settings.cluster);
+  const { cluster, updateLookupTableAddresses } = useJetStore(state => ({
+    cluster: state.settings.cluster,
+    updateLookupTableAddresses: state.updateLookupTableAddresses
+  }));
   const setMainConfig = useSetRecoilState(MainConfig);
 
   async function getLocalnetConfig() {
@@ -20,13 +23,24 @@ export function useMainConfigSyncer() {
     return await response.data;
   }
 
+  async function getAuthorityLookupTables(authority: string): Promise<string[]> {
+    return (await axios.get<{
+      authority: string,
+      addresses: string[]
+    }>(`http://localhost:3006/lookup/authority_addresses/${authority}`)).data.addresses
+  }
+
   useEffect(() => {
     if (cluster == 'localnet') {
       getLocalnetConfig().then(config => {
         setMainConfig(config);
+        return getAuthorityLookupTables(config.airspaces[0].lookupRegistryAuthority)
+      }).then(addresses => {
+        updateLookupTableAddresses(addresses);
       });
     } else {
       MarginClient.getConfig(cluster).then(config => setMainConfig(config));
+      // TODO: update authority here too
     }
   }, [cluster, setMainConfig]);
 }
