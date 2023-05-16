f46ac53d6df3efb87e6c3ed791ca9a0d1ab835af
Davide Scalzo <scalzodavide@yahoo.it>
2023-05-16 11:30:47 +0100

    geoblocking stub (#708)
    * geoblocking stub
    
    * disable check on localhost for CI purporses
    
    * fix import

--- apps/react-app/src/state/settings/localization/localization.ts
+++ apps/react-app/src/state/settings/localization/localization.ts
@@ -1,8 +1,10 @@
-import { atom } from 'recoil';
+import { atom, selector } from 'recoil';
 import axios from 'axios';
 import Jet_UI_EN from './languages/Jet_UI_EN.json';
 import Jet_Definitions_EN from './languages/Jet_Definitions_EN.json';
 import { localStorageEffect } from '../../effects/localStorageEffect';
+import { WalletPublicKey } from '@state/user/accounts';
+import { isDebug } from '../../../App';
 
 // UI Localization Dictionary
 export const Dictionary = atom({
@@ -17,34 +19,92 @@ export const PreferredLanguage = atom({
   effects: [localStorageEffect('jetAppPreferredLanguage')]
 });
 
+interface GeoLocation {
+  ip: string;
+  type: string;
+  location: {
+    city: string;
+    continent: {
+      code: string;
+      name: string;
+    };
+    country: {
+      area: number;
+      borders: string[];
+      calling_code: string;
+      capital: string;
+      code: string;
+      name: string;
+    };
+    in_eu: boolean;
+    region: {
+      code: string;
+      name: string;
+    };
+  };
+  security: {
+    is_abuser: boolean;
+    is_anonymous: boolean;
+    is_attacker: boolean;
+    is_bogon: boolean;
+    is_cloud_provider: boolean;
+    is_proxy: boolean;
+    is_relay: boolean;
+    is_threat: boolean;
+    is_tor: boolean;
+    is_tor_exit: boolean;
+    is_vpn: boolean;
+  };
+  time_zone: {
+    id: string;
+  };
+  user_agent: {
+    header: string;
+    name: string;
+    os: {
+      name: string;
+      type: string;
+    };
+    version: string;
+  };
+}
+
 // Whether user is geobanned
-export const Geobanned = atom({
+export const Geobanned = selector<boolean>({
   key: 'geobanned',
-  default: false,
-  effects: [
-    ({ setSelf }) => {
-      axios
-        .get(`https://api.ipregistry.co/?key=${process.env.REACT_APP_IP_REGISTRY}`)
-        .then(({ data }) => {
-          const locale = data.data;
-          if (locale) {
-            const countryCode = locale.location.country.code;
-            if (countryCode) {
-              geoBannedCountries.forEach(c => {
-                if (c.code === countryCode) {
-                  // If country is Ukraine, checks if first two digits
-                  // of the postal code further match Crimean postal codes.
-                  if (countryCode !== 'UA' || locale.location.region.code === 'UA-43') {
-                    setSelf(true);
-                  }
-                }
-              });
-            }
-          }
-        })
-        .catch(err => err);
+  get: async ({ get }) => {
+    if (isDebug) {
+      return false;
     }
-  ]
+    const data = await axios
+      .get<GeoLocation>(`https://api.ipregistry.co/?key=${process.env.REACT_APP_IP_REGISTRY}`)
+      .then(({ data }) => data);
+
+    const countryCode = data.location.country.code;
+    const tz = data.time_zone.id;
+    const wallet = get(WalletPublicKey);
+    if (wallet && process.env.REACT_APP_ALLOWED_WALLETS?.includes(wallet)) {
+      // is whitelisted
+      return false;
+    }
+
+    if (geoBannedCountries.map(country => country.code).includes(countryCode)) {
+      // Is in a geobanned country
+      return true;
+    }
+
+    if (Object.values(data.security).some(v => v)) {
+      // is masking traffic
+      return true;
+    }
+
+    if (tz !== Intl.DateTimeFormat().resolvedOptions().timeZone) {
+      // Timezone is different from that of the IP resolved one
+      return true;
+    }
+
+    return false;
+  }
 });
 
 // UI dictionary
@@ -94,6 +154,10 @@ export const geoBannedCountries = [
     code: 'KP'
   },
   {
+    country: 'Russia',
+    code: 'RU'
+  },
+  {
     country: 'Sudan',
     code: 'SD'
   },
@@ -104,6 +168,14 @@ export const geoBannedCountries = [
   {
     country: 'Tajikistan',
     code: 'TJ'
+  },
+  {
+    country: 'Ukraine',
+    code: 'UA'
+  },
+  {
+    country: 'United States',
+    code: 'US'
   },
   {
     country: 'Venezuela',
