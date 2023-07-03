35a42637cf6ee819e2d45d26608e1ffa211a6144
KevinRafaelConnor <kevinrafaelconnor@gmail.com>
2023-07-03 09:54:49 -0700

    adding audit reports link and small wording update

--- apps/react-app/src/components/misc/TermsPrivacy.tsx
+++ apps/react-app/src/components/misc/TermsPrivacy.tsx
@@ -2,13 +2,19 @@ import { useRecoilValue } from 'recoil';
 import { Dictionary } from '@state/settings/localization/localization';
 import { Typography } from 'antd';
 
-// Terms of Use, Privacy Policy and Definition links
+// Terms of Use, Privacy Policy, Glossary, and Audit Reports links
 export function TermsPrivacy(): JSX.Element {
   const dictionary = useRecoilValue(Dictionary);
   const { Paragraph } = Typography;
 
   return (
     <div className="terms-privacy flex-centered">
+      <a
+        href="https://docs.jetprotocol.io/jet-protocol/audit-reports"
+        target="_blank"
+        rel="noopener noreferrer">
+        <Paragraph className="text-btn">{dictionary.termsPrivacy.audits}</Paragraph>
+      </a>
       <a href="https://www.jetprotocol.io/legal/terms-of-service" target="_blank" rel="noopener noreferrer">
         <Paragraph className="text-btn">{dictionary.termsPrivacy.termsOfService}</Paragraph>
       </a>
